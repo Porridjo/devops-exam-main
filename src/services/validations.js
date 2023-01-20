f658beb49535aa64ac28e3053afd96b84808497e
Porridjo <Den.nguyen69@gmail.com>
2023-01-20 10:06:32 +0100

    test(services): add test case short label

--- src/services/validations.js
+++ src/services/validations.js
@@ -1,5 +1,9 @@
 // TODO: Add here the function to validate
 
+function validateGamerTag(tag) {
+    return false;
+}
+
 const isEmpty = (label) => !label || label.length === 0;
 
-exports.isEmpty = isEmpty;
+module.exports = { isEmpty, validateGamerTag };
